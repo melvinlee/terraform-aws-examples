ba842374a82f7298714f983201f9ac6af33795f7
melvinlee <melvinlee@hotmail.com>
2021-05-03 16:30:38 +0800

    added advanced_security_options.

--- elasticsearch-single-az/elasticsearch.tf
+++ elasticsearch-single-az/elasticsearch.tf
@@ -30,6 +30,28 @@ resource "aws_elasticsearch_domain" "this" {
     "rest.action.multi.allow_explicit_index" = "true"
   }
 
+  advanced_security_options {
+    enabled                        = true
+    internal_user_database_enabled = true
+    master_user_options {
+      master_user_name     = "esadmin"
+      master_user_password = random_password.es-password.result
+    }
+  }
+
+  node_to_node_encryption {
+    enabled = true
+  }
+
+  encrypt_at_rest {
+    enabled = true
+  }
+
+  domain_endpoint_options {
+    enforce_https       = true
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
+  }
+
   access_policies = <<CONFIG
 {
     "Version": "2012-10-17",
